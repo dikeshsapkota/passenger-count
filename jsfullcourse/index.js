3c94d2cc4cbf2553edceb961c5b37c29afb37525
Dikesh Sapkota <dikeshsapkota@Dikeshs-MacBook-Pro.local>
2025-12-23 16:02:51 +0545

    after hitting save count resets to 0

--- jsfullcourse/index.js
+++ jsfullcourse/index.js
@@ -5,13 +5,15 @@ console.log("button clicked");
 count+=1;
 document.getElementById("count").innerText= count;
 console.log(count);
+
 }
 console.log("save-El")
 function save(){
     console.log("saved");
     let save=count;
     console.log(save);
-    let countStr= count +"-"
+  
+    let countStr= count +" - "
     document.getElementById("save-El").textContent += countStr;
-    
+document.getElementById("count").innerText=0;
 }
