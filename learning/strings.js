1938a4e19018e50e600fada09b3972b7f8aac702
Dikesh Sapkota <dikeshsapkota@Dikeshs-MacBook-Pro.local>
2025-12-24 11:38:48 +0545

    black jack first prototype using if-else DOM and function

---
+++ learning/strings.js
@@ -0,0 +1,7 @@
+let age=20;
+if(age>=21){
+    console.log("welcome");
+}
+else{
+    console.log("not allowed")
+}
--- strings.js
+++
@@ -1,5 +0,0 @@
-let welcomeEl =document.getElementById("welcome-el")
-let name="Dikesh Sapkota";
-let greeting="welcome back";
-welcomeEl.innerText=greeting+ name;
-welcomeEl.innerText += "👋" ;
