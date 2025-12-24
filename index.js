05dbca8c7725be95e0dd382baed394e4299f75eb
Dikesh Sapkota <dikeshsapkota@Dikeshs-MacBook-Pro.local>
2025-12-24 13:13:45 +0545

    Add passenger-count project

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+
 let count=0;
 function increment(){
 console.log("button clicked");
 count+=1;
 document.getElementById("count").innerText= count;
 console.log(count);
-}
 
+}
+console.log("save-El")
 function save(){
     console.log("saved");
     let save=count;
     console.log(save);
+  
+    let countStr= count +" - "
+    document.getElementById("save-El").textContent += countStr;
+document.getElementById("count").innerText=0;
+count=0;
 }
